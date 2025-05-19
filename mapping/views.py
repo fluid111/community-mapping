c76d3a815b13113c9b5e38f958dcc3c6ba2fe8d1
fluid-coder <130135533+fluid111@users.noreply.github.com>
2025-05-19 09:07:59 +0545

    deleteMarkerMode unncessary code deleted

--- mapping/views.py
+++ mapping/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
 
+
 from rest_framework.decorators import api_view
 
 from django.shortcuts import render
